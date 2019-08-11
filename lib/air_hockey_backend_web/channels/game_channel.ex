f4bdfb1c767f8f0eb92acaf432848941a95a21c8
Rinbo <robin.b@outlook.com>
2019-08-11 22:56:21 +0200

    Add ping handle in function in game channel

--- lib/air_hockey_backend_web/channels/game_channel.ex
+++ lib/air_hockey_backend_web/channels/game_channel.ex
@@ -2,16 +2,18 @@ defmodule AirHockeyBackendWeb.GameChannel do
   use AirHockeyBackendWeb, :channel
 
   def join("game:" <> game_name, %{"player_name" => name }, socket) do
-    #first check that it is the player that created the channel    
-    send(self(), {:add_game_to_lobby, game_name })
-    {:ok, assign(socket, :game_name, game_name)}
-  end
+    IO.puts("=============")
+    IO.inspect(socket)
+    IO.puts("=============")
 
-  def handle_info({:add_game_to_lobby, game_name}, socket) do
-    AirHockeyBackendWeb.Endpoint.broadcast_from!(self(), "lobby:main",
-    "game_created", %{game_name: game_name})
+   {:ok, assign(socket, :lobby, name)}
+  end
+  
+  def handle_in("ping", _payload, socket) do
+    IO.puts("=============")
+    IO.inspect(socket)
+    IO.puts("=============")
     {:noreply, socket}
   end
-
 
 end
