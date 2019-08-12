56566b36eca985f536340a4000d8a74d3d1e982f
Rinbo <robin.b@outlook.com>
2019-08-12 23:44:37 +0200

    Trigger game_created handle_info function upon making a channel topic

--- lib/air_hockey_backend_web/channels/game_channel.ex
+++ lib/air_hockey_backend_web/channels/game_channel.ex
@@ -4,19 +4,29 @@ defmodule AirHockeyBackendWeb.GameChannel do
   alias AirHockeyBackendWeb.Presence
 
   def join("game:" <> game_name, %{"player_name" => name }, socket) do
-    send(self(), {:after_join, game_name})
+    send(self(), {:after_join, name})
+    unless game_name == "lobby" do
+      send(self(), {:topic_created, game_name})
+    end
     {:ok, socket}
   end
 
-  def handle_info({:after_join, game_name}, socket) do
-    {:ok, _} = Presence.track(socket, game_name, %{online_at: inspect(System.system_time(:seconds))})
+  def handle_info({:after_join, name}, socket) do
+    {:ok, _} = Presence.track(socket, name, %{online_at: inspect(System.system_time(:seconds))})
     {:noreply, socket}
   end
 
+  def handle_info({:topic_created, game_name}, socket) do
+    
+    IO.puts("NEW GAME CREATED")
+    IO.inspect(socket)
+    IO.puts("NEW GAME CREATED")
+    {:noreply, socket} 
+  end
   
   def handle_in("ping", _payload, socket) do
     IO.puts("=============")
-    IO.inspect(Presence.list())
+    IO.inspect(socket)
     IO.puts("=============")
     {:noreply, socket}
   end
--- lib/air_hockey_backend_web/controllers/ChannelController.ex
+++ lib/air_hockey_backend_web/controllers/ChannelController.ex
@@ -2,9 +2,12 @@ defmodule AirHockeyBackendWeb.ChannelController do
   use AirHockeyBackendWeb, :controller
 
   def index(conn, params) do
-    IO.puts("=============")
-    IO.inspect(conn)
-    IO.puts("=============")
+    games = Supervisor.which_children(AirHockeyBackend.Supervisor)
+
+    IO.puts("=====")
+    IO.inspect(games)
+    IO.puts("=====")
+
     json(conn, %{channels: "No channels here"})
   end
 end
