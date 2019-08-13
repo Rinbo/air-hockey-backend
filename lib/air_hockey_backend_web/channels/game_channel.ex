e8211576d68804342b059b2945bc49217cf47051
Rinbo <robin.b@outlook.com>
2019-08-13 22:07:32 +0200

    Add abliity to ping websocket and see all active unique games

--- lib/air_hockey_backend_web/channels/game_channel.ex
+++ lib/air_hockey_backend_web/channels/game_channel.ex
@@ -4,10 +4,7 @@ defmodule AirHockeyBackendWeb.GameChannel do
   alias AirHockeyBackendWeb.Presence
 
   def join("game:" <> game_name, %{"player_name" => name }, socket) do
-    send(self(), {:after_join, name})
-    unless game_name == "lobby" do
-      send(self(), {:topic_created, game_name})
-    end
+    send(self(), {:after_join, name})    
     {:ok, socket}
   end
 
@@ -16,30 +13,32 @@ defmodule AirHockeyBackendWeb.GameChannel do
     #{:noreply, socket}
 
     :ok = Phoenix.PubSub.subscribe(
-             socket.pubsub_server,
-             "subtopic_listing",
-             fastlane: {socket.transport_pid, socket.serializer, []}
-           )
-    {:ok, _} = Presence.track(self, "subtopic_listing", name, %{
-      online_at: inspect(System.system_time(:second)),
-      topic: socket.topic
-    })
-    {:noreply, socket}
-  end
+              socket.pubsub_server,
+              "subtopic_listing",
+              fastlane: {socket.transport_pid, socket.serializer, []}
+            )
+      
 
-  def handle_info({:topic_created, game_name}, socket) do
-    
-    IO.puts("NEW GAME CREATED")
-    IO.inspect(socket)
-    IO.puts("NEW GAME CREATED")
-    {:noreply, socket} 
+    {:ok, _} = Presence.track(self(), "subtopic_listing", name, %{online_at: inspect(System.system_time(:seconds)), topic: socket.topic})
+    {:noreply, socket}
   end
   
   def handle_in("ping", _payload, socket) do
     IO.puts("=============")
-    IO.inspect(socket)
+    IO.inspect(list_active_unique_games())
     IO.puts("=============")
     {:noreply, socket}
+  end
+
+  defp list_active_unique_games() do
+    Presence.list("subtopic_listing")
+    |> Map.values
+    |> Enum.map(fn element -> 
+      %{ metas: [%{online_at: _, phx_ref: _, topic: "game:" <> g_name}]} = element
+      g_name
+    end)
+    |> Enum.uniq
+    |> Enum.filter(fn game_name -> game_name != "lobby" end)    
   end
 
 end
