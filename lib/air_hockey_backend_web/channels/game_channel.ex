46f1a71b726619c953136aec641182d4f8777f17
Rinbo <robin.b@outlook.com>
2019-08-17 11:30:19 +0200

    Modify list games function to also return how many players are currently in the subtopic

--- lib/air_hockey_backend_web/channels/game_channel.ex
+++ lib/air_hockey_backend_web/channels/game_channel.ex
@@ -3,7 +3,7 @@ defmodule AirHockeyBackendWeb.GameChannel do
 
   alias AirHockeyBackendWeb.Presence
 
-  def join("game:" <> game_name, %{"player_name" => name }, socket) do
+  def join("game:" <> _game_name, %{"player_name" => name }, socket) do
     send(self(), {:after_join, name})    
     {:ok, socket}
   end
@@ -13,29 +13,34 @@ defmodule AirHockeyBackendWeb.GameChannel do
               socket.pubsub_server,
               "subtopic_listing",
               fastlane: {socket.transport_pid, socket.serializer, []}
-            )
-      
-
+            )      
     {:ok, _} = Presence.track(self(), "subtopic_listing", name, %{online_at: inspect(System.system_time(:seconds)), topic: socket.topic})
     {:noreply, socket}
   end
   
   def handle_in("get_active_games", _payload, socket) do
     IO.puts("This function was called")
-    games = list_active_unique_games()
+    games = list_games_with_player_count()
     broadcast!(socket, "active_games", %{games: games})
     {:noreply, socket}
   end
 
-  defp list_active_unique_games() do
-    Presence.list("subtopic_listing")
+  defp list_games_with_player_count() do 
+    total_entries = Presence.list("subtopic_listing")
     |> Map.values
     |> Enum.map(fn element -> 
       %{ metas: [%{online_at: _, phx_ref: _, topic: "game:" <> g_name}]} = element
       g_name
     end)
-    |> Enum.uniq
-    |> Enum.filter(fn game_name -> game_name != "lobby" end)    
+    |> Enum.filter(fn game_name -> game_name != "lobby" end)
+
+    unique_entries = Enum.uniq(total_entries)
+
+    Enum.map(unique_entries, fn x ->
+      tot = Enum.count(total_entries, fn y -> y == x end)
+      %{x => tot}
+    end)
+    
   end
 
 end
